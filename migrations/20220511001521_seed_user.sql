e2e2398571f919cd9716935b27ce787682e2d24a
Alex van de Sandt <alex@avandesa.dev>
2022-05-10 20:25:07 -0400

    Add seed user

---
+++ migrations/20220511001521_seed_user.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES
+    (
+        '5448d448-3566-4ecb-9dc7-081eeb040ca1',
+        'admin',
+        '$argon2id$v=19$m=15000,t=2,p=1$EK8FOXU/xZ3NFIHo9/Gmmw$nK4hkwWHSHyHugQMlGLLfBYLki1WztYAQW5qS3qpfmk'
+    )
